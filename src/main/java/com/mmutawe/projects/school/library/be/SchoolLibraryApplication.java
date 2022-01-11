68184c21f3f4abbfe304a4dd3f15f643da821a66
mmutawe <mahmoudmutawe@gmail.com>
2022-01-11 11:01:33 -0600

    Initial commit

---
+++ src/main/java/com/mmutawe/projects/school/library/be/SchoolLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mmutawe.projects.school.library.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolLibraryApplication.class, args);
+	}
+
+}
