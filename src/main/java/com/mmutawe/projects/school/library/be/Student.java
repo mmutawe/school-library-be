66ae87a387f9fa7c3886b2b138832dca613c5add
mmutawe <mahmoudmutawe@gmail.com>
2022-01-11 14:26:42 -0600

    replace imports * with single class imports

--- src/main/java/com/mmutawe/projects/school/library/be/Student.java
+++ src/main/java/com/mmutawe/projects/school/library/be/Student.java
@@ -1,6 +1,13 @@
 package com.mmutawe.projects.school.library.be;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity(name = "Student")
 @Table(
