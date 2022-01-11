e2c0d4ab3ff503fd860c4e2e4331a6c70705d882
mmutawe <mahmoudmutawe@gmail.com>
2022-01-11 14:23:42 -0600

    implement student jpa entity & students table in postgreDB

---
+++ src/main/java/com/mmutawe/projects/school/library/be/Student.java
@@ -0,0 +1,123 @@
+package com.mmutawe.projects.school.library.be;
+
+import javax.persistence.*;
+
+@Entity(name = "Student")
+@Table(
+        name = "students",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "student_email_unique",
+                        columnNames = "email"
+                )
+        }
+)
+public class Student {
+
+    // id: unique & not nullable by default
+    @Id
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+
+    @Column(
+            name = "first_name",
+            columnDefinition = "TEXT",
+            nullable = false
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            columnDefinition = "TEXT",
+            nullable = false
+    )
+    private String lastName;
+
+    @Column(
+            name = "email",
+            columnDefinition = "TEXT",
+//            unique = true,
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String firstName, String lastName, String email, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
